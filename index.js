d9cfda4fd68a6335dcf2a5d596edc62c95b46bb4
magda-gierg <mjgiergiczny@gmail.com>
2018-05-25 18:03:51 +1200

    changeScale is working

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const loc = document.getElementById("location");
-const temNumber = document.getElementById("temperature_number");
-const temScale = document.getElementById("temperature_scale");
+const tempNumber = document.getElementById("temperature_number");
+const tempScale = document.getElementById("temperature_scale");
 const weatherCondition = document.getElementById("weather_condition");
 const weatherIcon = document.getElementById("weather_icon");
 
 window.onload = function() {
   getLocation();
 };
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -18,20 +19,30 @@ function getLocation() {
 }
 // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
 
-
-
 function getWeather(lat, long) {
   const root = "https://fcc-weather-api.glitch.me/api/current?";
   fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson.name);
-    loc.innerHTML = myJson.name;
-    temNumber.innerHTML = myJson.main.temp;
-    weatherCondition.innerHTML = myJson.weather[0].main;
-    weatherIcon.innerHTML = `<img src="${myJson.weather[0].icon}" />`
-  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+      loc.innerHTML = myJson.name;
+      tempNumber.innerHTML = myJson.main.temp;
+      weatherCondition.innerHTML = myJson.weather[0].main;
+      weatherIcon.innerHTML = `<img src="${myJson.weather[0].icon}"/>`;
+    });
 }
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+function changeScale() {
+  if (tempScale.innerHTML === "C") {
+    tempNumber.innerHTML = Math.round(tempNumber.innerHTML * 9 / 5 + 32);
+    tempScale.innerHTML = "F";
+    console.log(tempScale.innerHTML);
+  } else {
+    tempNumber.innerHTML = Math.round((tempNumber.innerHTML - 32) * 5 / 9);
+    tempScale.innerHTML = "C";
+  }
+}
+tempScale.addEventListener("click", changeScale);
